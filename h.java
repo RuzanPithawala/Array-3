7dbbef4829b3ac7644c043d27c546495051d6bab
Ruzan Pithawala <36655798+RuzanPithawala@users.noreply.github.com>
2024-11-30 20:11:44 -0500

    Create h.java

---
+++ h.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n=citations.length;
+        Arrays.sort(citations);
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(citations[mid]==n-mid) return n-mid;
+            else if(citations[mid]<n-mid){
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+        return n-low;
+    }
+}
